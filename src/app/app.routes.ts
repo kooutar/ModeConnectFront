7ed6caaf8498790f3362f39335fda218695ac5bc
kooutar <kawtarlaajil7@gmail.com>
2026-03-01 12:24:04 +0000

    feat: scaffold home page with shared navbar/footer components and global Tailwind styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegistrationComponent } from './features/auth/pages/registration-component/registration-component';
 import { LoginComponent } from './features/auth/pages/login-component/login-component';
+import { HomeComponent } from './features/home/pages/home-component/home-component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component:RegistrationComponent
-    },
-    {
-      
-        path: 'login',
-        component:LoginComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
